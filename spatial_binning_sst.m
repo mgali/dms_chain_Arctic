c83a0ebc0b5e7c2ac6855b06c97ed7212f60a983
martigalitapias <marti.gali.tapias@gmail.com>
2017-01-26 13:21:04 -0500

    Little correction to spatial_binning_sst.m

--- spatial_binning_sst.m
+++ spatial_binning_sst.m
@@ -1,14 +1,8 @@
-% SPATIAL BINNING TESTS WITH MATLAB
+% SPATIAL BINNING OF ERA-Interim SST DATA
 clear
 
 dateoutformat = 'YYYYDDD'; % YYYYDDD or YYYYMMDD
-
-% Data files
-years = 1998:2015;
-varnames = {'sst'};
-% basedatapath = '~/Desktop/Geophysical_data/ERA-Interim/SST/'; baseoutpath = basedatapath; % FOR TESTS
-basedatapath = '/Volumes/rap/martigalitapias/ERA-Interim/SST/'; % on taku-leifr
-baseoutpath = '/Volumes/rap/martigalitapias/ERA-Interim/SST_28km/'; % on taku-leifr
+outformat = 'txt'; % 'nc' or 'txt'
 
 % Grids
 kmgrid2 = '28'; % 28, 37 or 46
@@ -17,7 +11,14 @@ grid1name = 'ERAi_45N.mat';
 load([gridpath grid1name]); grid1 = [lon lat]; clear lon; clear lat
 grid2name = ['grid' kmgrid2 'km_45N.txt'];
 grid2 = dlmread([gridpath grid2name],' ');
+npixels2 = size(grid2,1);
 
+% Data files
+years = 1998:2015; % 1998:2015;
+varnames = {'sst'};
+% basedatapath = '~/Desktop/Geophysical_data/ERA-Interim/SST/'; baseoutpath = basedatapath; % FOR TESTS
+basedatapath = '/Volumes/rap/martigalitapias/ERA-Interim/SST/'; % on taku-leifr
+baseoutpath = sprintf('/Volumes/rap/martigalitapias/ERA-Interim/SST_%skm/',kmgrid2); % on taku-leifr
 
 %% Use index list to bin dmspt from grid1 to grid2
 
@@ -31,11 +32,11 @@ toc
 for vv = 1:length(varnames)
     for yyyy = years
         
-        datapath = [basedatapath num2str(yyyy) '/']; % uncomment for taku-leifr
+        datapath = [basedatapath num2str(yyyy) '/'];
         flist = dir([datapath '/*.nc']);
         
         % Select days (eg first of 8D period)
-        flist(mod(1:size(flist,1),8)~=1) = [];
+        % flist(mod(1:size(flist,1),8)~=1) = [];
         
         for mmdd = 1:length(flist)
             
@@ -44,21 +45,8 @@ for vv = 1:length(varnames)
             datain = ncread(filepath,varnames{vv});
             datain = datain(:) - 273.15;
             
-            %             h = figure(); clf, set(gcf,'units','centimeters','position',[5 20 40 20])
-            %             subplot(1,2,1)
-            %             plot_sst_stereo(datain,8,grid1)
-            
             % Binning/reprojection
-            dataout = nan(nrows,1);
-            for rr = 1:nrows
-                dataout(rr,:) = nanmean(datain(indlist{rr,1}));
-                if ~mod(rr,10000), rr, toc, end
-            end
-            tmp = [yyyy mmdd nanmean(dataout)];
-            
-            %             subplot(1,2,2)
-            %             dataout(isnan(dataout)) = 10;
-            %             plot_sst_stereo(dataout,8,grid2)
+            dataout = repbin_grid1_grid2(datain,indlist);
             
             % Write file
             dataout(isnan(dataout)) = -999;
@@ -67,20 +55,36 @@ for vv = 1:length(varnames)
             datename = dummy2{1,2};
             
             if strcmp(dateoutformat,'YYYYMMDD')
-                %             outpath = sprintf('%s%s_%s.txt',baseoutpath,varnames{vv},datename); % TESTS
-                outpath = sprintf('%s%i/%s_%s.txt',baseoutpath,yyyy,varnames{vv},datename); % taku-leifr
+                outpath = sprintf('%s%i/%s_%s.%s',baseoutpath,yyyy,varnames{vv},datename,outformat); % taku-leifr
             elseif strcmp(dateoutformat,'YYYYDDD')
                 Y = str2double(datename(1:4));
                 M = str2double(datename(5:6));
                 D = str2double(datename(7:8));
                 DOY = yearday(D,M,Y,0,0,0);
-                outpath = sprintf('%s%0.0f/%s_%0.0f%03.0f.txt',baseoutpath,Y,varnames{vv},Y,DOY); % taku-leifr
+                outpath = sprintf('%s%0.0f/%s_%0.0f%03.0f.%s',baseoutpath,Y,varnames{vv},Y,DOY,outformat); % taku-leifr
             end
-            dlmwrite(outpath,dataout,'\t');
             
-            %             dlmwrite('./summary_MeanSstDay.txt',tmp,'-append');
+            if strcmp(outformat,'txt')
+                dlmwrite(outpath,dataout,'\t');
+            elseif strcmp(outformat,'nc')
+                nccreate(outpath,varnames{vv},'format','netcdf4','Dimensions',{'r' npixels2 'c' 1});
+                ncwrite(outpath,varnames{vv},dataout);
+            end
             
-            %             print(h,sprintf('~/Desktop/Heintzenberg_Leck_collaboration/SST_maps/SST_%s.png',datename),'-dpng');
+            % Store mean temperature in separate file
+            % tmp = [yyyy mmdd nanmean(dataout)];
+            % dlmwrite('./summary_MeanSstDay.txt',tmp,'-append');
+            
+            % % Plot side by side datain and dataout, save
+            % h = figure(); clf, set(gcf,'units','centimeters','position',[5 20 40 20])
+            % subplot(1,2,1)
+            % plot_sst_stereo(datain,8,grid1)
+            % subplot(1,2,2)
+            % % dataout(isnan(dataout)) = 10;
+            % plot_sst_stereo(dataout,8,grid2)
+            % % print(h,sprintf('~/Desktop/Heintzenberg_Leck_collaboration/SST_maps/SST_%s.png',datename),'-dpng');
+            % print(h,sprintf('%sSST_%s_ERAvs%skm.png',datapath,datename,kmgrid2),'-dpng');
+            % close(h)
             
         end
     end
